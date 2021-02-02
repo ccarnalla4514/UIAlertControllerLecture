cc585d64a4a1a70671dc61a0f58ddc4bfd13636b
Christian Carnalla <ccarnalla4514@stu.d214.org>
2021-02-02 15:01:28 -0600

    UIAlert lecture

--- UIAlertControllerLecture/ViewController.swift
+++ UIAlertControllerLecture/ViewController.swift
@@ -11,9 +11,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
     }
 
+    @IBAction func onAddButtonTapped(_ sender: Any) {
+        let alert = UIAlertController(title: "Is it OK?", message: nil, preferredStyle: UIAlertController.Style.alert)
+        
+        let ok = UIAlertAction(title: "OK", style: .default) { (action) in
+            print("User Tapped on OK")
+        }
+            let no = UIAlertAction(title: "No", style: .default) { (action) in
+                print("User Tapped on No")
+            
+        }
+        alert.addAction(ok)
+        alert.addAction(no)
+    
+        
+    present(alert, animated: true, completion: nil)
+    }
+    }
+    
 
-}
+    
 
