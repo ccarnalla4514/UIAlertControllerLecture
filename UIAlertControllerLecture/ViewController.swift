062c0babac0c7ac7607fe528e439ef5ca4c1ec95
Christian Carnalla <ccarnalla4514@stu.d214.org>
2021-02-02 14:36:00 -0600

    Initial Commit

---
+++ UIAlertControllerLecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIAlertControllerLecture
+//
+//  Created by Christian Carnalla on 2/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
